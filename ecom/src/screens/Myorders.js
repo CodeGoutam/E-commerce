7de7418c04b1e8cd7c9b1609f2e69fee4ede4c50
Hemant Goutam <93316509+CodeGoutam@users.noreply.github.com>
2024-02-17 16:23:42 +0530

    Update Myorders.js

--- ecom/src/screens/Myorders.js
+++ ecom/src/screens/Myorders.js
@@ -5,7 +5,7 @@ const Myorders = () => {
     let res;
     const [data, setData] = useState([])
     const fetchingFunction = async () => {
-        const response = await fetch(`http://localhost:5000/api/myorders`,
+        const response = await fetch(`https://e-commerce-navy-pi.vercel.app/api/myorders`,
             {
                 mode: 'no-cors',
                 method: "POST",
