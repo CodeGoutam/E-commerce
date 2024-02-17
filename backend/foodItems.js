09c84d1bedfe83259611311aad36447b79a43c3d
Hemant Goutam <93316509+CodeGoutam@users.noreply.github.com>
2024-02-17 20:55:53 +0530

    Update foodItems.js

--- backend/foodItems.js
+++ backend/foodItems.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 let foodItems, foodCat;
 async function display() {
     // await mongoose.connect("mongodb://localhost:27017/gofood");
-    await mongoose.connect("mongodb+srv://hgoutam2001:ecommerce@cluster0.pzsxlbg.mongodb.net/Ecommerce?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://hgoutam2001:ecommerce@cluster0.pzsxlbg.mongodb.net/gofood?retryWrites=true&w=majority");
     console.log("db connectedd");
     const items = mongoose.connection.db.collection("food_items");
     foodItems = await items.find({}).toArray();
