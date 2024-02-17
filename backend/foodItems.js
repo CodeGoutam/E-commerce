d0d51f933deffea1deaf978d6296053a435b9206
CodeGoutam <hgoutam2001@gmail.com>
2024-02-17 15:55:36 +0530

    adding files

---
+++ backend/foodItems.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+let foodItems, foodCat;
+async function display() {
+    // await mongoose.connect("mongodb://localhost:27017/gofood");
+    await mongoose.connect("mongodb+srv://gofood:gofood@cluster0.qjptync.mongodb.net/gofood?retryWrites=true&w=majority");
+    console.log("db connectedd");
+    const items = mongoose.connection.db.collection("food_items");
+    foodItems = await items.find({}).toArray();
+    const category = mongoose.connection.db.collection("food_category");
+    foodCat = await category.find({}).toArray();
+}
+display();
+router.post("/fooditem", (req, res) => {
+    try {
+        res.send([foodItems, foodCat]);
+        console.log(foodItems)
+    } catch (e) {
+        console.log(e);
+    }
+    console.log("fooditems")
+});
+module.exports = router;
---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const cors = require('cors')
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", `http://localhost:3000`);
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+// });
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+app.use(express.json());
+express.urlencoded({ extended: true });
+app.use("/api", require("./foodItems"));
+app.use("/api", require("./signUp"));
+app.use("/api", require("./signIn"));
+app.use("/api", require("./Orders"));
+app.use("/api", require("./Myorders"));
+app.use("/", (req, res) => {
+    res.send("Backend ")
+    console.log("backend")
+})
+
+app.listen(5000,()=>{
+    console.log("server is running")
+});
