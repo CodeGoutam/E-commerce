98a7d23b3dad6ea6b6a57ca09bea51c750a1d268
CodeGoutam <hgoutam2001@gmail.com>
2024-02-17 22:39:25 +0530

    updates

--- backend/foodItems.js
+++ backend/foodItems.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-let foodItems, foodCat;
-async function display() {
-    // await mongoose.connect("mongodb://localhost:27017/gofood");
-    await mongoose.connect("mongodb+srv://hgoutam2001:ecommerce@cluster0.pzsxlbg.mongodb.net/gofood?retryWrites=true&w=majority");
-    console.log("db connectedd");
-    const items = mongoose.connection.db.collection("food_items");
-    foodItems = await items.find({}).toArray();
-    const category = mongoose.connection.db.collection("food_category");
-    foodCat = await category.find({}).toArray();
-}
+
+
+
 display();
-router.post("/fooditem", (req, res) => {
+router.post("/fooditem", async(req, res) => {
     try {
+        const items = mongoose.connection.db.collection("food_items");
+    const category = mongoose.connection.db.collection("food_category");
+        let foodItems = await items.find({}).toArray();
+        let foodCat = await category.find({}).toArray();
+
         res.send([foodItems, foodCat]);
         console.log(foodItems)
     } catch (e) {
