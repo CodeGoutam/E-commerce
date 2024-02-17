d93c976ec3f03f13d621f16709bf17d0519ea588
Hemant Goutam <93316509+CodeGoutam@users.noreply.github.com>
2024-02-17 21:30:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require('cors')
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", `http://localhost:3000`);
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-// });
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", `http://localhost:3000`);
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 app.use(cors(
     {
         origin: ["https://e-commerce-f-one.vercel.app"],
